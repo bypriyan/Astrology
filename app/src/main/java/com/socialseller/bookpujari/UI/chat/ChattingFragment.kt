b23cd936eb294fe76e20681120dedeb5087c4d7e
priyanshu <104priyanshu@gmail.com>
2025-04-12 15:41:33 +0530

    payment screen

---
+++ app/src/main/java/com/socialseller/bookpujari/UI/chat/ChattingFragment.kt
@@ -0,0 +1,44 @@
+package com.socialseller.bookpujari.UI.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.socialseller.bookpujari.R
+import com.socialseller.bookpujari.UI.home.HomeActivity
+import com.socialseller.bookpujari.databinding.FragmentChattingBinding
+
+
+class ChattingFragment : Fragment(R.layout.fragment_chatting) {
+
+    private var _binding: FragmentChattingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentChattingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as? HomeActivity)?.hideBottomNavigation()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        (activity as? HomeActivity)?.showBottomNavigation()
+    }
+}
